4b64b63cc4d32f7b855e8dbbb05c33b65513895c
Philip Wong <110500922+ThatBoiisback@users.noreply.github.com>
2023-03-05 18:50:56 -0500

    Imported random + defined chooseGesture with random module

--- ai.py
+++ ai.py
@@ -1,5 +1,10 @@
 from player import Player
+import random
 
 class AI(Player):
     def __init__(self, gestures):
         Player.__init__(self, 'AI', gestures)
+
+    def chooseGesture(self):
+        gestureVal =  random.randint(0,4)
+        self.gesture = self.gestures[gestureVal]
