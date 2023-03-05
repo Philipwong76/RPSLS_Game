4c005baba4b05587fd220a8fd7773ee80172fec0
Philip Wong <110500922+ThatBoiisback@users.noreply.github.com>
2023-03-05 18:55:43 -0500

    Created conditionals to include player2

--- game.py
+++ game.py
@@ -29,3 +29,27 @@ class gameplay:
                 if AIPlayer.points == 3:
                     winnerFound = True
                     print("AI wins")
+
+        elif numPlayers == '2':
+            player1 = Player("Player 1",gestures)
+            player2 = Player("Player 2",gestures)
+            while not winnerFound:
+                print("Possible gestures: ", gestures)
+                playerGesture = player1.chooseGesture()
+                player1.gesture = playerGesture
+                playerGesture = player2.chooseGesture()
+                player2.gesture = playerGesture
+
+                result = (player1.gesture, player2.gesture)
+                if result == 1:
+                    player1.points += 1
+                elif result == 2:
+                    player2.points += 1
+                
+                if player1.points == 3:
+                    winnerFound = True
+                    print("Player 1 wins")
+
+                if player2.points == 3:
+                    winnerFound = True
+                    print("Player 2 wins")
