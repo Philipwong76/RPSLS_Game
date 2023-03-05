fb385963aaa5d64cf2f728dab460c2a43b48a8b3
Philip Wong <110500922+ThatBoiisback@users.noreply.github.com>
2023-03-05 18:49:07 -0500

    Defined chooseGesture in player.py

--- player.py
+++ player.py
@@ -4,3 +4,7 @@ class Player:
         self.points = 0
         self.gestures = gestures
         self.gesture = ""
+
+    def chooseGesture(self):
+        while self.gesture not in self.gestures:
+            self.gesture = input("What gesture do you choose?")
